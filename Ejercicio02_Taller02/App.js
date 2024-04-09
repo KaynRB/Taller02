517a2ad18c5869252163140774f500ae5a9bd9c0
RB230288 <125234995+KaynRB@users.noreply.github.com>
2024-04-08 22:12:47 -0600

    Update App.js

--- Ejercicio02_Taller02/App.js
+++ Ejercicio02_Taller02/App.js
@@ -1,38 +1,37 @@
+import React from 'react';
 import { StyleSheet, Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { Ionicons } from '@expo/vector-icons';
 import Tipicos from './Screens/Tipicos';
 import Sopas from './Screens/Sopas';
 import CarneMarisco from './Screens/CarneMarisco';
-export default function App(){
+export default function App() {
   const Tab = createBottomTabNavigator();
-  const TabNavigator = () =>(
-    <Tab.Navigator
-    screenOptions={({ route }) => ({
-      tabBarInactiveTintColor: "#f48b28",
-      tabBarActiveTintColor: "#633204",
-      tabBarIcon: ({ focused, color, size }) => {
-      let iconName;
-      if (route.name === 'Screen1') {
-      iconName = Platform.OS === 'ios' ? 'home-outline' : 'home';
-      } else if (route.name === 'Screen2') {
-      iconName = Platform.OS === 'ios' ? 'list-outline' : 'list';
-      }
-      return <Ionicons name={iconName} size={size} color={color} />;
-    },
-    })}
-    >
-    <Tab.Screen name="Tipicos" component={Tipicos}/>
-    <Tab.Screen name="Sopas" component={Sopas}/>
-    <Tab.Screen name="Carne/Mariscos" component={CarneMarisco}/>
-    </Tab.Navigator>
-  );
-  return(
-  <NavigationContainer>
-  <TabNavigator/>
-  </NavigationContainer>
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarInactiveTintColor: "#f48b28",
+          tabBarActiveTintColor: "#633204",
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Tipicos') {
+              iconName = 'pizza';
+            } else if (route.name === 'Sopas') {
+              iconName = 'color-fill';
+            } else if (route.name === 'Carne/Mariscos') {
+              iconName = 'fish';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Tipicos" component={Tipicos} />
+        <Tab.Screen name="Sopas" component={Sopas} />
+        <Tab.Screen name="Carne/Mariscos" component={CarneMarisco} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
